1a5f6ec905403799d353a2fe7aaf2689434f7077
Derek <legato_5@yahoo.com>
2019-11-07 19:14:47 -0800

    set up basic structure to app. need to set up testing.

---
+++ web-application-testing/src/Components/ScoreBoard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ScoreBoard = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default ScoreBoard;
